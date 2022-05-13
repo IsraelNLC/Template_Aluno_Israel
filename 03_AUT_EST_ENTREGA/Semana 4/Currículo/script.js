687a4639cb300c4858f67d041da9fbfc4ace2b77
Israel <israel.carvalho@sou.inteli.edu.br>
2022-05-13 18:05:42 -0300

    Atualizei o currículo para a semana 4

---
+++ 03_AUT_EST_ENTREGA/Semana 4/Currículo/script.js
@@ -0,0 +1,22 @@
+var click = false
+
+function display(){
+    if(click === false){
+        createniver();
+        click = true;
+    }
+    else{
+        document.getElementById("newp").remove();
+        click = false;
+    }
+
+}
+
+function createniver(){
+    const paragraph = document.createElement("p");
+    const niver = document.createTextNode("Oii, a última atualização foi feita no dia do meu aniversário!! Você descobriu um easter egg ;)")
+    paragraph.setAttribute("id", "newp")
+    paragraph.appendChild(niver)
+
+    document.getElementById("resumo").appendChild(paragraph);
+}
--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/exercicio_obrigatorio.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 3/exercicio_obrigatorio.js
@@ -7,3 +7,19 @@ function ex(){
     ts = v0/g;
     document.getElementById("results").innerHTML = "A altura máxima será de " + hmax + " e o tempo de subida será de " + ts
 }
+
+function popup(){
+     var popup = prompt("Digite um valor");
+     if (typeof popup === "string"){
+         console.log("oi");
+         document.getElementById("value").innerHTML = "O valor de texto indicado é: " + popup}
+    else if (typeof popup === "Int"){
+        document.getElementById("value").innerHTML = "O valor numérico indicado é: " + popup}
+}
+
+function changeimg(source){
+    document.getElementById("mario").src = source
+}
+
+// Quero que ele detecte se o que eu dei no popup foi um número ou um texto, e se for número, transformar de string pra int/float pra ativar o elif
+// ParseInt() ou parseFloat() muda o tipo da var
